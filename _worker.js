b8de80b1e15f369a21db53167d76ca1ce78d4359
akram228 <95872995+akram228@users.noreply.github.com>
2026-02-14 10:41:17 +0630

    _worker.js

--- _worker.js
+++ _worker.js
@@ -1,2 +1,8 @@
-// ptvpn v1.0 - Created by Phoe Thar
 // @ts-nocheck
+// ptvpn v1.0 - Created by Phoe Thar
+// Last Update: 2024-05-24
+
+import { connect } from 'cloudflare:sockets';
+// How to generate your own UUID:
+// https://www.uuidgenerator.net/
+let userID = '0eadd30d-4335-4abf-ba50-97a110f6adbe';
