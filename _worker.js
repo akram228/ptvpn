e04dd0a16849567b37311856a4d202b9db95cace
akram228 <95872995+akram228@users.noreply.github.com>
2026-02-14 19:03:48 +0630

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,3 +6,6 @@ import { connect } from 'cloudflare:sockets';
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
 let userID = '0eadd30d-4335-4abf-ba50-97a110f6adbe';
+
+// https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
+// https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
